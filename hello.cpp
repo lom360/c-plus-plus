6e09a5b34bfa3d7acc573ff1cdbdf338c07dae20
lom360 <lom360@mail.fresnostate.edu>
2017-10-02 00:53:13 -0700

    Added hello.cpp
    Also added a compiler project where I will use to debug or just compile
    my code.

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int main(){
+    string hi = "Hello, world";
+   
+    cout << hi << endl;
+
+    system("pause");
+    return 0;
+}
