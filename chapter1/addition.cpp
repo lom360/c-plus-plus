61b25a2e25866ea675e14f83bdf8d0b68c69668f
lom360 <lom360@mail.fresnostate.edu>
2017-10-02 01:13:17 -0700

    removed compiler project

---
+++ chapter1/addition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	int a = 5;
+	int b = 9;
+	int ans = 0;
+
+	ans = a + b;
+
+	cout << ans << endl;
+
+	system("pause");
+	return 0;
+}
