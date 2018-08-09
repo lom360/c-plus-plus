9f6f9bf0a842fb648c364beff5765570ba4669bd
lom360 <lom360@mail.fresnostate.edu>
2018-08-08 22:34:17 -0700

    Starting MergeSort.cpp file

---
+++ Algorithms/MergeSort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#define SIZE 10
+using namespace std;
+
+int main()
+{
+    int arr[SIZE] = {0, 11, 24, 32, 45, 58, 67, 79, 84, 93};
+
+    system("pause");
+    return 0;
+}
