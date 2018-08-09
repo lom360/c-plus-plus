1b7f7636dcdb84d2c937e0982b23ce09fd94e5d0
lom360 <lom360@mail.fresnostate.edu>
2018-08-08 23:06:06 -0700

    Created MergeSort function

--- Algorithms/MergeSort.cpp
+++ Algorithms/MergeSort.cpp
@@ -3,10 +3,29 @@
 #define SIZE 10
 using namespace std;
 
+void Merge(int arr[], int left, int mid, int right);
+void MergeSort(int arr[], int left, int right);
+
 int main()
 {
     int arr[SIZE] = {0, 11, 24, 32, 45, 58, 67, 79, 84, 93};
 
     system("pause");
     return 0;
+}
+
+void Merge(int arr[], int left, int mid, int right)
+{
+}
+
+void MergeSort(int arr[], int left, int right)
+{
+    int mid = (left + right) / 2;
+
+    if (left < right)
+    {
+        MergeSort(arr, left, mid);
+        MergeSort(arr, mid + 1, right);
+        Merge(arr, left, mid, right);
+    }
 }
