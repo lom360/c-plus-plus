392053aa6e73b63dc2c54dbf75e88e44060cb0db
lom360 <lom360@mail.fresnostate.edu>
2018-08-08 23:50:21 -0700

    Need to fix array issue within Merge function

--- Algorithms/MergeSort.cpp
+++ Algorithms/MergeSort.cpp
@@ -10,12 +10,22 @@ int main()
 {
     int arr[SIZE] = {0, 11, 24, 32, 45, 58, 67, 79, 84, 93};
 
+    MergeSort(arr, 0, SIZE);
+
     system("pause");
     return 0;
 }
 
 void Merge(int arr[], int left, int mid, int right)
 {
+    const int leftSize = mid - 1;
+    const int rightSize = right - mid;
+    int leftArray[leftSize];
+    int rightArray[rightSize];
+
+    for (int i = 0; i < mid; i++)
+    {
+    }
 }
 
 void MergeSort(int arr[], int left, int right)
