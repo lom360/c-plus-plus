681e2f8d84c3bfa18912ec36202eae68c4c8566d
lom360 <lom360@mail.fresnostate.edu>
2018-11-29 20:28:29 -0800

    Created Shapes.cpp which prints out a triangle

---
+++ C/Shapes.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	// Here we are just building a shape.
+	// For the most part each line statement will read in order,
+	// but as we move on we will see how different code will affect
+	// how or when each line will be executed.
+	printf("   /|\n");
+	printf("  / |\n");
+	printf(" /  |\n");
+	printf("/___|\n");
+
+	system("pause");
+	return 0;
+}
