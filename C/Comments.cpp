88ec0a3d3cadacb7d4b1150525606f8c25dcf410
lom360 <lom360@mail.fresnostate.edu>
2018-11-30 23:38:46 -0800

    Went over the topic of comments

---
+++ C/Comments.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	/*
+	 * Commments are ignored by the compiler during execution.
+	 * Comments can be helpful when you need to omit code to experiement or test.
+	 * Comments are also used to explain lines of code.
+	 */
+
+	//printf("Comments are fun");
+
+	system("pause");
+	return 0;
+}
